3ae69acef6ad8734e6048183a1eee0f007556e18
Dongsik0000 <panda1842@naver.com>
2025-01-24 00:42:18 +0900

    feat: 특정 게시글 조회 기능 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './boards.entity';
 
@@ -13,6 +13,13 @@ export class BoardsController {
 	    return this.boardsService.getAllBoards();
     }
 
+    // 특정 게시글 조회 기능
+    @Get('/:id')
+    getBoardDetailByID(
+        @Param('id') id: number): Board {
+        return this.boardsService.getBoardDetailById(id);
+    }
+
     // 게시글 작성 기능
     @Post('/')
     createBoard(
@@ -20,6 +27,6 @@ export class BoardsController {
         @Body('title') title: string,
         @Body('contents') contents: string,
     ) {
-        return this.boardsService.createBoard(author, title, contents)
+        return this.boardsService.createBoard(author, title, contents);
     }
 }

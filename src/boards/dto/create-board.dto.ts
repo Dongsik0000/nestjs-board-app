b744e0357de590eac634cb9cfee7c1277f11878f
Dongsik0000 <panda1842@naver.com>
2025-01-27 17:29:08 +0900

    refactor: 게시글 작성 기능에 유효성검사 파이프 추가

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class CreateBoardDto {
+    @IsNotEmpty() // null값 체크
+    @IsString() // 문자열 체크
     author : string;
+
+    @IsNotEmpty()
+    @IsString()
     title: string;
+
+    @IsNotEmpty()
+    @IsString()
     contents: string;
 }
